f77aba6a790663dfde533883b08112a8a0a7ad63
Gyan-Ponnappa-1 <gyanopcon@gmail.com>
2024-08-03 08:54:35 +0530

    2

--- app/src/main/java/com/example/decentralisedresourceshare/Constants.kt
+++ app/src/main/java/com/example/decentralisedresourceshare/Constants.kt
@@ -2,6 +2,6 @@ package com.example.decentralisedresourceshare
 
 val ACCOUNTS = "accounts"
 val NODES = "nodes"
-val SAVEDNODES = "SavedNodes"
+val SAVEDNODES = "approvedNode"
 val ALLPICS = "AllPics"
 val APIKEY = "AIzaSyBByxYhe7D_CWbCyrbPiBn3g4XfCo3bznE"
